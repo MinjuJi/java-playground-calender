6e67105b16d018a2ec72daadafc71f60ef44f057
도토리 <@DESKTOP-UMGB17U>
2023-08-19 08:22:13 +0900

    메소드로 설정

--- src/playground/Calender.java
+++ src/playground/Calender.java
@@ -4,40 +4,43 @@ import java.util.Scanner;
 
 public class Calender {
 
-	public static void main(String[] args) {
-//		System.out.println("일 월 화 수 목 금 토");
-//		System.out.println("--------------------");
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
+	private int getMaxDayOfMonth(int month) {
 
-		
 		// 윤년 제외
-		Scanner kb = new Scanner(System.in);
-		
-		System.out.print("월 입력: ");
-		int num = kb.nextInt();
-		
-		if ( num <= 7 && num >= 3 || num == 1) {
-			if(num%2 == 0) {
-				System.out.println(30);
-			}else {
-				System.out.println(31);
+		if (month <= 7 && month >= 3 || month == 1) {
+			if (month % 2 == 0) {
+				return 30;
+			} else {
+				return 31;
 			}
-		}else if( num > 7 ) {
-			if(num%2 == 0) {
-				System.out.println(31);
-			}else {
-				System.out.println(30);
+		} else if (month > 7) {
+			if (month % 2 == 0) {
+				return 31;
+			} else {
+				return 30;
 			}
-		}else if(num == 2){
-			System.out.println(28);
-		}else {
-			System.out.println("1 ~ 12 사이 숫자를 입력하세요.");
+		} else if (month == 2) {
+			return 28;
 		}
-		
-			kb.close();
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner kb = new Scanner(System.in);
+
+		System.out.print("월 입력: ");
+		int month = kb.nextInt();
+		kb.close();
+
+		if (month < 1 || month > 12) {
+			System.out.println("1 ~ 12 사이 숫자를 입력해주세요.");
+		} else {
+			Calender calender = new Calender();
+			int maxDay = calender.getMaxDayOfMonth(month);
+			System.out.println(maxDay);
+		}
+
 	}
 
 }
