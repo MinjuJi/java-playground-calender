9387fe57a9c75c9520dfdafa09ccce5bf10ee2c2
도토리 <@DESKTOP-UMGB17U>
2023-08-20 07:20:16 +0900

    CalenderApp 프로그램 생성 시 참고한 프로그램

---
+++ src/playground/DayOfCalenderApp.java
@@ -0,0 +1,22 @@
+// 프로그램: 연도와 월을 입력 받아 [일요일 ~ 월요일] 순의 달력 생성 프로그램 
+
+package playground;
+
+import java.util.Scanner;
+
+public class DayOfCalenderApp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("연도 입력 : ");
+		int year = sc.nextInt();
+
+		System.out.print("월 입력 : ");
+		int month = sc.nextInt();
+
+		DayOfCalender.prn(year, month);
+
+		sc.close();
+	}
+}
